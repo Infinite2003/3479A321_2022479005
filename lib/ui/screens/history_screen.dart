a081a14f015ee48ef4531cca963e95175e7359b5
Infinite2003 <elbastian2003@gmail.com>
2026-04-22 01:06:07 -0400

    Implementacion de Historial Usando Card

--- lib/ui/screens/history_screen.dart
+++ lib/ui/screens/history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../models/game_result.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({Key? key}) : super(key: key);
@@ -6,10 +7,27 @@ class HistoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
+    final GameResult game = GameResult(
+      date: '10 octubre', 
+      timeSpent: '1:10', 
+      isVictory: true);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Historial (Maqueta)')),
-      body: const Center(
-        child: Text('Aqui ira la lista de partidas'),
+      body: Center(
+        child: Card(
+          elevation: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Fecha: ${game.date}'),
+                const SizedBox(height: 8),
+                Text('Tiempo: ${game.timeSpent}'),
+              ],
+            ),),
+        ),
       ),
     );
   }
