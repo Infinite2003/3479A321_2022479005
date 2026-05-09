62c2c3f977abe0e4f29cdd522c5f88552616ac97
Infinite2003 <elbastian2003@gmail.com>
2026-05-09 00:20:28 -0400

    Cambio a StatefulWidget

--- lib/ui/screens/minesweeper_screen.dart
+++ lib/ui/screens/minesweeper_screen.dart
@@ -10,45 +10,102 @@ class MinesweeperScreen extends StatefulWidget {
   const MinesweeperScreen({Key? key}) : super(key: key);
 
   @override
+  State<MinesweeperScreen> createState() => _MinesweeperScreenState();
+}
+
+class _MinesweeperScreenState extends State<MinesweeperScreen> {
+
+  late List<CellModel> _cells;
+
+  final logger = Logger();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _cells = List.generate(
+      64,
+      (i) => CellModel(index: i),
+    );
+
+    logger.i('Lifecycle: initState()');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    logger.i('Lifecycle: didChangeDependencies()');
+  }
+
+  @override
+  void didUpdateWidget(covariant MinesweeperScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    logger.w('Lifecycle: didUpdateWidget()');
+  }
+
+  @override
+  void dispose() {
+    logger.e('Lifecycle: dispose()');
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
 
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    logger.i('Lifecycle: build()');
 
-    final String difficulty = args?['difficulty'] ?? 'Desconocida';
-    final int gridSize = args?['gridSize'] ?? 8;
+    final args =
+        ModalRoute.of(context)?.settings.arguments
+            as Map<String, dynamic>?;
+
+    final String difficulty =
+        args?['difficulty'] ?? 'Desconocida';
+
+    final int gridSize =
+        args?['gridSize'] ?? 8;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Buscaminas'),
-        actions: [
-        
-        IconButton(
-          icon: const Icon(Icons.history),
-          tooltip: 'Ver Historial',
-          onPressed: (){
-            Navigator.pushNamed(context, '/history');
-          },),
 
-        IconButton(
-          icon: const Icon(Icons.menu),
-          tooltip: 'Ir al Menu',
-          onPressed: (){
-            Navigator.pushNamed(context, '/menu');
-          },
+        actions: [
+
+          IconButton(
+            icon: const Icon(Icons.history),
+            tooltip: 'Ver Historial',
+
+            onPressed: () {
+              Navigator.pushNamed(context, '/history');
+            },
           ),
-      ],
+
+          IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: 'Ir al Menu',
+
+            onPressed: () {
+              Navigator.pushNamed(context, '/menu');
+            },
+          ),
+        ],
       ),
-      
+
       body: SafeArea(
         child: Column(
           children: [
+
             Container(
               height: 60,
               color: Colors.grey[300],
+
               child: Center(
                 child: Text(
                   'Dificultad: $difficulty | Tamaño: $gridSize x $gridSize',
-                  style: TextStyle(
+
+                  style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 18,
                   ),
